2b741a3d7c9d43816540c8fd0ea78368e10cb76b
Rafael <Rafaelaleman1988@gmail.com>
2018-10-24 21:34:38 +0200

    Facturas del cliente

--- spring-boot-data-jpa/src/main/java/com/example/springboot/app/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/models/entity/Cliente.java
@@ -1,15 +1,21 @@
 package com.example.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
-import javax.persistence.Temporal; 
+import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
@@ -18,77 +24,103 @@ import javax.validation.constraints.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
-@Table(name="clientes")
-public class Cliente implements Serializable{
+@Table(name = "clientes")
+public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotEmpty
 	private String nombre;
-	
+
 	@NotEmpty
 	private String apellidos;
-	
+
 	@NotEmpty
 	@Email
 	private String email;
-	
+
 	@NotNull
-	@Column(name="create_at")
+	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
-	 
-	/* Se llama automaticamente antes de persistir (persist)
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
-	*/
+
+	@OneToMany(mappedBy="cliente", fetch=FetchType.LAZY, cascade=CascadeType.ALL) /* Un cliente tiene muchas facturas */
+	private List<Factura> facturas;
+
+	/*
+	 * Se llama automaticamente antes de persistir (persist)
+	 * 
+	 * @PrePersist public void prePersist() { createAt = new Date(); }
+	 */
 	private String foto;
-	
+
+	public Cliente() {
+		this.facturas = new ArrayList<Factura>();
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellidos() {
 		return apellidos;
 	}
+
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Date getCreateAt() {
 		return createAt;
 	}
+
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
+
 	public String getFoto() {
 		return foto;
 	}
+
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
-	
-	
-	
-	
+
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+
+	public void agregarFactura(Factura factura) {
+		facturas.add(factura);
+	}
+
 }
