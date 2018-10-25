4aeff2c666faa17c801ae1365bfc6f0d87b2220a
Rafael <Rafaelaleman1988@gmail.com>
2018-10-25 11:06:47 +0200

    Links acceso a factura

---
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/controllers/FacturaController.java
@@ -0,0 +1,44 @@
+package com.example.springboot.app.controllers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.springboot.app.models.entity.Cliente;
+import com.example.springboot.app.models.entity.Factura;
+import com.example.springboot.app.models.service.IClienteService;
+
+@Controller
+@RequestMapping("/factura")
+@SessionAttributes("factura") /* el nombre es el mismo nombre de factura que se pasa a la vista */
+public class FacturaController {
+
+	@Autowired
+	private IClienteService clienteService;
+
+	@GetMapping("/form/{clienteId}")
+	public String crear(@PathVariable(value = "clienteId") Long clienteId, Map<String, Object> model,
+			RedirectAttributes flash) {
+
+		Cliente cliente = clienteService.findOne(clienteId);
+
+		if (cliente == null) {
+			flash.addFlashAttribute("error", "El cliente no existe en la base de datos!");
+			return "redirect:/listar";
+		}
+
+		Factura factura = new Factura();
+		factura.setCliente(cliente);
+
+		model.put("factura", factura);
+		model.put("titulo", "Crear factura");
+
+		return "factura/form";
+	}
+}
