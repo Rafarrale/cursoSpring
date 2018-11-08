412a431fbd1c82c00a7e86db82bb5b9fb533abf2
rafarrale <rafarrale@localhost>
2018-11-08 11:43:18 +0100

    SPRING SECURITY: 102
    Añadimos dependecias y creamos la clase de configuracion
    SpringSecurityConfig donde preparamos todo para un sistema de
    autentificacion en memoria

--- spring-boot-data-jpa/src/main/java/com/example/springboot/app/MvcConfig.java
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/MvcConfig.java
@@ -1,17 +1,13 @@
 package com.example.springboot.app;
 
-import java.nio.file.Paths;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@Configuration
-public class MvcConfig extends WebMvcConfigurationSupport{
+public class MvcConfig implements WebMvcConfigurer{
 	
-	private Logger log = LoggerFactory.getLogger(getClass());
+	//private Logger log = LoggerFactory.getLogger(getClass());
 	/*
 	@Override
 	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
