df7ad66154e38c9ff2a293b630a63a2ece08ca2e
Rafa <rarroyoaleman@gmail.com>
2019-02-08 17:35:52 +0100

    save

--- spring-boot-data-jpa/src/main/java/com/example/springboot/app/SpringSecurityConfig.java
+++ spring-boot-data-jpa/src/main/java/com/example/springboot/app/SpringSecurityConfig.java
@@ -3,15 +3,40 @@ package com.example.springboot.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.example.springboot.app.auth.handler.LoginSuccessHandler;
+
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private LoginSuccessHandler successHandler;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		/* Primero paginas publicas "USERS, ADMIN Y ANONIMOS" */
+		http.authorizeRequests().antMatchers("/", "/css/**", "/jss/**", "/images/**", "/listar").permitAll()
+		.antMatchers("/ver/**").hasAnyRole("USER")
+		.antMatchers("/uploads/**").hasAnyRole("USER")
+		.antMatchers("/form/**").hasAnyRole("ADMIN")
+		.antMatchers("/eliminar/**").hasAnyRole("ADMIN")
+		.antMatchers("/factura/**").hasAnyRole("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+			.formLogin()
+				.defaultSuccessUrl("/")
+				.loginPage("/login")
+			.permitAll()
+		.and()
+		.logout().permitAll();
+	}
+
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception
 	{
